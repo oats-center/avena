de05dcdf0dfdda5765cce0b460beea8e9f09df83
Andrew Balmos <andrew@balmos.org>
2025-05-16 22:20:08 -0400

    Missing files
    Signed-off-by: Andrew Balmos <andrew@balmos.org>

---
+++ avenad/src/systemd/unit.rs
@@ -0,0 +1,50 @@
+use zbus::proxy;
+
+#[proxy(
+    interface = "org.freedesktop.systemd1.Unit",
+    default_service = "org.freedesktop.systemd1",
+    default_path = "/org/freedesktop/systemd/unit"
+)]
+pub trait Systemd1Unit {
+    #[zbus(property)]
+    fn id(&self) -> zbus::Result<String>;
+
+    #[zbus(property)]
+    fn active_state(&self) -> zbus::Result<String>;
+
+    #[zbus(property)]
+    fn need_daemon_reload(&self) -> zbus::Result<bool>;
+
+    // #[dbus_proxy(property, name = "Type")]
+    // fn service_type(&self) -> zbus::Result<String>;
+    //
+    // #[dbus_proxy(property)]
+    // fn status_text(&self) -> zbus::Result<String>;
+    //
+    // #[dbus_proxy(property)]
+    // fn memory_current(&self) -> zbus::Result<u64>;
+    //
+    // #[dbus_proxy(property)]
+    // fn memory_available(&self) -> zbus::Result<u64>;
+    //
+    // #[dbus_proxy(property)]
+    // fn memory_accounting(&self) -> zbus::Result<bool>;
+    //
+    // #[dbus_proxy(property, name = "CPUUsageNSec")]
+    // fn cpu_usage_n_sec(&self) -> zbus::Result<u64>;
+    //
+    // #[dbus_proxy(property, name = "CPUShares")]
+    // fn cpu_shares(&self) -> zbus::Result<u64>;
+    //
+    // #[dbus_proxy(property, name = "CPUAccounting")]
+    // fn cpu_accounting(&self) -> zbus::Result<bool>;
+    //
+    // #[dbus_proxy(property)]
+    // fn exec_main_start_timestamp(&self) -> zbus::Result<u64>;
+    //
+    // #[dbus_proxy(property)]
+    // fn exec_main_exit_timestamp(&self) -> zbus::Result<u64>;
+    //
+    // #[dbus_proxy(property, name = "ExecMainPID")]
+    // fn exec_main_pid(&self) -> zbus::Result<u32>;
+}
